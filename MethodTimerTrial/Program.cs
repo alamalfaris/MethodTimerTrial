02cf82c8ae5f6f1e9de1d618d2cba4ec697046c2
Alam Alfaris <alamalfaris97@gmail.com>
2023-12-31 08:51:24 +0700

    Add project files.

---
+++ MethodTimerTrial/Program.cs
@@ -0,0 +1,32 @@
+﻿using MethodTimer;
+
+namespace MethodTimerTrial
+{
+    internal class Program
+    {
+        [Time]
+        static void Main(string[] args)
+        {
+            Loop10();
+            Loop1000();
+        }
+
+        [Time]
+        static void Loop10()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        [Time]
+        static void Loop1000()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
